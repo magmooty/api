daff39615a2fd260f48819fd01d1881b5d171ae4
Ziad Alzarka <ziadalzarka@gmail.com>
2022-08-24 01:58:25 +0200

    chore: modify redeploy script to delete old images

--- redeploy.sh
+++ redeploy.sh
@@ -3,5 +3,4 @@
 git fetch
 git reset --hard
 git pull
-cd docker-compose/dev && docker-compose up -d --build app
-#docker rm $(docker ps -q -f 'status=exited') && docker rmi $(docker images -q -f "dangling=true")
+cd docker-compose/dev && docker-compose up -d --build app && docker rmi $(docker images -q -f "dangling=true")
