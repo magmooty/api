78924c1cb3866d5b5d42ca9f883371fbcb688d84
Ziad Alzarka <ziadalzarka@gmail.com>
2022-08-24 01:53:37 +0200

    chore: modify redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd docker-compose/dev && docker-compose up -d --build app
-docker rm $(docker ps -q -f 'status=exited')
-docker rmi $(docker images -q -f "dangling=true")
+cd docker-compose/dev && docker-compose up -d --build app && docker rm $(docker ps -q -f 'status=exited') && docker rmi $(docker images -q -f "dangling=true")
