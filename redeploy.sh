ecbad9b8266b77dc4f03beb7f1a4bbc9ef4dd8e8
Ziad Alzarka <ziadalzarka@gmail.com>
2022-08-24 00:32:45 +0200

    chore: add redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-cd docker-compose/$1 && docker-compose up -d --build app
+
+rm package-lock.json
+npm install
+npm run build
+
+output=$(pm2 pid app)
+
+if [ -n "$output" ]; then
+  pm2 restart app
+else
+  pm2 start npm --name app -- start
+fi
