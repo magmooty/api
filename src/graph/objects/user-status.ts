dc73605d7894e71d1ca7924b9166e97decc0b5d7
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-03 21:06:35 +0200

    feat: support user status model

---
+++ src/graph/objects/user-status.ts
@@ -0,0 +1,36 @@
+import { ObjectConfig } from "@/graph";
+import { OwnerViewVirtualExecutor } from "@/graph/common";
+
+export default {
+  code: "Z4",
+  cacheLevel: "external",
+  views: {
+    _default: {
+      GET: ["virtual:owner"],
+      POST: [],
+      PATCH: [],
+    },
+  },
+  virtuals: {
+    views: {
+      owner: {
+        pre: ["all"],
+        execute: OwnerViewVirtualExecutor,
+      },
+    },
+  },
+  fields: {
+    status: {
+      type: "value-set",
+      valueSet: "user-status",
+    },
+    set_at: {
+      type: "date",
+    },
+    set_by: {
+      type: "object-id",
+      objectTypes: ["user"],
+    },
+  },
+  edges: {},
+} as ObjectConfig;
