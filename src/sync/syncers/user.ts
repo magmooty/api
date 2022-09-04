9b60d4530d1e0b6acab5b80dfd84be612b61e249
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-04 21:12:31 +0200

    fix: user name syncer

--- src/sync/syncers/user.ts
+++ src/sync/syncers/user.ts
@@ -24,6 +24,7 @@ const universalUserGenerator = (
           humanName.middle_name || "",
           humanName.last_name || "",
         ]
+          .filter((term) => term)
           .map((str) => str.trim())
           .join(" ")
       )
