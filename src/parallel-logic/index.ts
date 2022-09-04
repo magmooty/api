ea0ac5ad43aad46908333345b082ab411bb94b25
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-04 19:17:15 +0200

    fix: parallel-logic executor

--- src/parallel-logic/index.ts
+++ src/parallel-logic/index.ts
@@ -2,6 +2,7 @@ import { queue, wrapper } from "@/components";
 import { GraphObject, IEdge } from "@/graph/objects/types";
 import { QueueEvent } from "@/queue";
 import { Context } from "@/tracing";
+import parallelLogicTriggers from "./triggers";
 
 export class ParallelLogic {
   init = wrapper({ name: "init", file: __filename }, (ctx: Context) => {
@@ -10,8 +11,15 @@ export class ParallelLogic {
 
   processEvent = wrapper(
     { name: "processEvent", file: __filename },
-    (ctx: Context, event: QueueEvent<GraphObject | IEdge>) => {
+    async (ctx: Context, event: QueueEvent<GraphObject | IEdge>) => {
       ctx.register({ event });
+
+      const handler: keyof typeof parallelLogicTriggers =
+        `${event.method} ${event.path}` as any;
+
+      if (parallelLogicTriggers[handler]) {
+        await parallelLogicTriggers[handler](ctx, event);
+      }
     }
   );
 }
