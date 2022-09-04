f638e3546a5eda59923c73df1b09637478a43fee
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-04 20:30:28 +0200

    chore: send version in telegram message

--- send-telegram-message.sh
+++ send-telegram-message.sh
@@ -3,5 +3,6 @@ ID="-1001715042430"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
 BRANCH=$(cat config.json | grep "\"env\":" | sed 's/  "env": "//' | sed 's/",//')
+VERSION=$(cat package.json | grep "\"version\":" | sed 's/  "version": "//' | sed 's/",//')
 
-curl -s -X POST $URL -d chat_id=$ID -d text="Build has finished for Magmooty on ${BRANCH}"
+curl -s -X POST $URL -d chat_id=$ID -d text="Build has finished for Magmooty on ${BRANCH} version ${VERSION}"
