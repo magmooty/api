d02ed872174c9b84b384801ad69e921cbaf2002c
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-04 20:27:00 +0200

    chore: fix redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -4,4 +4,5 @@ git fetch
 git reset --hard
 git pull
 cd docker-compose/dev && docker-compose up -d --build app && docker rmi $(docker images -q -f "dangling=true")
+cd ../..
 ./send-telegram-message.sh
