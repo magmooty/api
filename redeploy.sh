ed13e0442fd549c64f08a48c3f569dcfa6269730
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-05 11:35:36 +0200

    chore: fix redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+git fetch
+git reset --hard
+git pull
+
 regex='\[version\]'
 text=$(git log -1 --pretty=%B)
 [[ $text =~ $regex ]]
 
 if [[ "$BASH_REMATCH[0]" == '[version]'* ]]; then
-git fetch
-git reset --hard
-git pull
 cd docker-compose/dev && docker-compose up -d --build app && docker rmi $(docker images -q -f "dangling=true")
 cd ../..
 ./send-telegram-message.sh
