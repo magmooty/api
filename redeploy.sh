7c7c4490d58552a75fd1833e4f720ab62bf541c4
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-06 23:51:36 +0200

    chore: rebuild version or merge commits

--- redeploy.sh
+++ redeploy.sh
@@ -4,11 +4,11 @@ git fetch
 git reset --hard
 git pull
 
-regex='\[version\]'
+regex='\[version\]|Merge'
 text=$(git log -1 --pretty=%B)
 [[ $text =~ $regex ]]
 
-if [[ "$BASH_REMATCH[0]" == '[version]'* ]]; then
+if [[ "$BASH_REMATCH[0]" == '[version]'* || "$BASH_REMATCH[0]" == 'Merge'* ]]; then
 cd docker-compose/dev && docker-compose up -d --build app && docker rmi $(docker images -q -f "dangling=true")
 cd ../..
 ./send-telegram-message.sh
