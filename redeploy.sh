57563e93082601fca37d943d27cb5755b134d43e
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-06 23:52:19 +0200

    chore: allow forced rebuild

--- redeploy.sh
+++ redeploy.sh
@@ -8,7 +8,7 @@ regex='\[version\]|Merge'
 text=$(git log -1 --pretty=%B)
 [[ $text =~ $regex ]]
 
-if [[ "$BASH_REMATCH[0]" == '[version]'* || "$BASH_REMATCH[0]" == 'Merge'* ]]; then
+if [[ $1 == "-f" || "$BASH_REMATCH[0]" == '[version]'* || "$BASH_REMATCH[0]" == 'Merge'* ]]; then
 cd docker-compose/dev && docker-compose up -d --build app && docker rmi $(docker images -q -f "dangling=true")
 cd ../..
 ./send-telegram-message.sh
