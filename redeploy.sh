0e01dcade743880e6de67931b14ccc86a88e4222
Ziad Alzarka <ziadalzarka@gmail.com>
2022-09-07 19:44:55 +0200

    chore: modify redeploy script to rebuild

--- redeploy.sh
+++ redeploy.sh
@@ -4,11 +4,11 @@ git fetch
 git reset --hard
 git pull
 
-regex='\[version\]|Merge'
+regex='\[version\]|Merge|rebuild'
 text=$(git log -1 --pretty=%B)
 [[ $text =~ $regex ]]
 
-if [[ $1 == "-f" || "$BASH_REMATCH[0]" == '[version]'* || "$BASH_REMATCH[0]" == 'Merge'* ]]; then
+if [[ $1 == "-f" || "$BASH_REMATCH[0]" == '[version]'* || "$BASH_REMATCH[0]" == 'Merge'* || "$BASH_REMATCH[0]" == 'rebuild'* ]]; then
 cd docker-compose/dev && docker-compose up -d --build app && docker rmi $(docker images -q -f "dangling=true")
 cd ../..
 ./send-telegram-message.sh
