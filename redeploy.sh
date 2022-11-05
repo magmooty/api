9f21ba7fd6e7926e2e1198eef9b01d3305fb5797
Ziad Alzarka <ziadalzarka@gmail.com>
2022-11-05 17:14:52 +0200

    [ci skip] chore: skip ci

--- redeploy.sh
+++ redeploy.sh
@@ -8,7 +8,7 @@ regex='\[version\]|rebuild'
 text=$(git log -1 --pretty=%B)
 [[ $text =~ $regex ]]
 
-if [[ $1 == "-f" || "$BASH_REMATCH[0]" == '[version]'* || "$BASH_REMATCH[0]" == 'rebuild'* ]]; then
+if [[ $1 == "-f" || "$BASH_REMATCH[0]" == '[ci skip] [version]'* || "$BASH_REMATCH[0]" == 'rebuild'* ]]; then
 cd docker-compose/dev && docker-compose up -d --build app && docker rmi $(docker images -q -f "dangling=true")
 cd ../..
 ./send-telegram-message.sh
